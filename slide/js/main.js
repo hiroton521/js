10139af49132e95a93cf54d52c2bf3cbb80ed95c
Hiroto Yamazaki <hiroton521@gmail.com>
2019-08-29 02:50:08 +0900

    Add files via upload

---
+++ slide/js/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+{
+  const images = [
+    'img/pic00.png',
+    'img/pic01.png',
+    'img/pic02.png',
+    'img/pic03.png',
+  ];
+
+  let currentNum = 0;
+
+  function setMainImage(image) {
+    document.querySelector('main img').src = image;
+  }
+
+  setMainImage(images[currentNum]);
+
+  const thumbnails = document.querySelector('.thumbnails');
+  images.forEach((image, index) => {
+    const li = document.createElement('li');
+    if (index === currentNum) {
+      li.classList.add('current');
+    }
+
+    li.addEventListener('click', () => {
+      setMainImage(image);
+    });
+
+    const img = document.createElement('img');
+    img.src = image;
+    li.appendChild(img);
+    thumbnails.appendChild(li);
+  });
+}
