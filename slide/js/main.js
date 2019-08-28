023ee70703a195f811b94363fcaeadafd6c8cf36
Hiroto Yamazaki <hiroton521@gmail.com>
2019-08-29 03:01:53 +0900

    Update main.js

--- slide/js/main.js
+++ slide/js/main.js
@@ -6,10 +6,6 @@
     'img/pic01.png',
     'img/pic02.png',
     'img/pic03.png',
-    'img/pic04.png',
-    'img/pic05.png',
-    'img/pic06.png',
-    'img/pic07.png',
   ];
 
   let currentNum = 0;
